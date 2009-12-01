924e5ab4fc699b6d9cf2c0676199e34908252ea1
Dennis Honeyman <arcticuno@gmail.com>
2009-12-01 13:33:11 -0500

    Added in style constants and a ttf_defs file.

---
+++ ttf/ttf_defs.go
@@ -0,0 +1,14 @@
+// godefs -g ttf ttf.c
+
+// MACHINE GENERATED - DO NOT EDIT.
+
+package ttf
+
+// Constants
+const (
+	STYLE_NORMAL = 0;
+	STYLE_BOLD = 0x1;
+	STYLE_ITALIC = 0x2;
+	STYLE_UNDERLINE = 0x4;
+)
+
