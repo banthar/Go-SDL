3cf4711f68320fd18a949ee1107be290894ec563
Dennis Honeyman <arcticuno@gmail.com>
2009-12-02 14:23:18 -0500

    Added Mix_LoadMUS and added a Music struct.

--- mixer/mixer.go
+++ mixer/mixer.go
@@ -6,8 +6,23 @@ package mixer
 // #include <SDL/SDL_mixer.h>
 import "C"
 
+type Music struct {
+	cmusic = *C.Mix_Music;
+}
+
 // Initializes SDL_mixer.
 func OpenAudio(frequency int, format uint16, channels int, chunksize int) int {
 	return int(C.Mix_OpenAudio(C.int(frequency), C.Uint16(format),
 		C.int(channels), C.int(chunksize)));
+}
+
+// Shuts down SDL_mixer.
+func CloseAudio() {
+	C.Mix_CloseAudio();
+}
+
+func LoadMUS(file string) *Music {
+	cfile := C.string(file);
+	cmusic := C.Mix_LoadMUS(cfile);
+	return &Music{cmusic};
 }
