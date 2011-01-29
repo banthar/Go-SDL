5b99e3526dfa8dab65d582e92608e9b3e6cc5382
Utkan Güngördü <utkan@freeconsole.org>
2011-01-29 20:53:16 +0200

    cleaned up transform module

--- transform/transform.go
+++ transform/transform.go
@@ -8,56 +8,6 @@ import (
 	"unsafe"
 )
 
-var shrink_X, shrink_Y, expand_X, expand_Y func(spixels, dpixels *byte, sh int32, spitch, dpitch uint16, sw, dw int32)
-
-func filter_shrink_X_MMX(spixels, dpixels *byte, sh int32, spitch, dpitch uint16, sw, dw int32) {
-	C.filter_shrink_X_MMX( (*C.Uint8)(spixels), (*C.Uint8)(dpixels), C.int(sh), C.int(spitch), C.int(dpitch), C.int(sw), C.int(dw))
-}
-
-func filter_shrink_Y_MMX(spixels, dpixels *byte, sh int32, spitch, dpitch uint16, sw, dw int32) {
-	C.filter_shrink_Y_MMX( (*C.Uint8)(spixels), (*C.Uint8)(dpixels), C.int(sh), C.int(spitch), C.int(dpitch), C.int(sw), C.int(dw))
-}
-
-func filter_expand_X_MMX(spixels, dpixels *byte, sh int32, spitch, dpitch uint16, sw, dw int32) {
-	C.filter_expand_X_MMX( (*C.Uint8)(spixels), (*C.Uint8)(dpixels), C.int(sh), C.int(spitch), C.int(dpitch), C.int(sw), C.int(dw))
-}
-
-func filter_expand_Y_MMX(spixels, dpixels *byte, sh int32, spitch, dpitch uint16, sw, dw int32) {
-	C.filter_expand_Y_MMX( (*C.Uint8)(spixels), (*C.Uint8)(dpixels), C.int(sh), C.int(spitch), C.int(dpitch), C.int(sw), C.int(dw))
-}
-
-func filter_shrink_X_SSE(spixels, dpixels *byte, sh int32, spitch, dpitch uint16, sw, dw int32) {
-	C.filter_shrink_X_SSE( (*C.Uint8)(spixels), (*C.Uint8)(dpixels), C.int(sh), C.int(spitch), C.int(dpitch), C.int(sw), C.int(dw))
-}
-
-func filter_shrink_Y_SSE(spixels, dpixels *byte, sh int32, spitch, dpitch uint16, sw, dw int32) {
-	C.filter_shrink_Y_SSE( (*C.Uint8)(spixels), (*C.Uint8)(dpixels), C.int(sh), C.int(spitch), C.int(dpitch), C.int(sw), C.int(dw))
-}
-
-func filter_expand_X_SSE(spixels, dpixels *byte, sh int32, spitch, dpitch uint16, sw, dw int32) {
-	C.filter_expand_X_SSE( (*C.Uint8)(spixels), (*C.Uint8)(dpixels), C.int(sh), C.int(spitch), C.int(dpitch), C.int(sw), C.int(dw))
-}
-
-func filter_expand_Y_SSE(spixels, dpixels *byte, sh int32, spitch, dpitch uint16, sw, dw int32) {
-	C.filter_expand_Y_SSE( (*C.Uint8)(spixels), (*C.Uint8)(dpixels), C.int(sh), C.int(spitch), C.int(dpitch), C.int(sw), C.int(dw))
-}
-
-func filter_shrink_X_ONLYC(spixels, dpixels *byte, sh int32, spitch, dpitch uint16, sw, dw int32) {
-// 	C.filter_shrink_X_ONLYC( (*C.Uint8)(spixels), (*C.Uint8)(dpixels), C.int(sh), C.int(spitch), C.int(dpitch), C.int(sw), C.int(dw))
-}
-
-func filter_shrink_Y_ONLYC(spixels, dpixels *byte, sh int32, spitch, dpitch uint16, sw, dw int32) {
-// 	C.filter_shrink_Y_ONLYC( (*C.Uint8)(spixels), (*C.Uint8)(dpixels), C.int(sh), C.int(spitch), C.int(dpitch), C.int(sw), C.int(dw))
-}
-
-func filter_expand_X_ONLYC(spixels, dpixels *byte, sh int32, spitch, dpitch uint16, sw, dw int32) {
-// 	C.filter_expand_X_ONLYC( (*C.Uint8)(spixels), (*C.Uint8)(dpixels), C.int(sh), C.int(spitch), C.int(dpitch), C.int(sw), C.int(dw))
-}
-
-func filter_expand_Y_ONLYC(spixels, dpixels *byte, sh int32, spitch, dpitch uint16, sw, dw int32) {
-// 	C.filter_expand_Y_ONLYC( (*C.Uint8)(spixels), (*C.Uint8)(dpixels), C.int(sh), C.int(spitch), C.int(dpitch), C.int(sw), C.int(dw))
-}
-
 func SmoothScaleBackend() string {
 	return C.GoString(C.get_smoothscale_backend())
 }
