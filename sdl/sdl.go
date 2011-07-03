6606cb7489fb4e1d5ce91d7cb229b412977503c8
DeedleFake <yisszev@beckforce.com>
2011-07-03 11:10:45 -0400

    Remove sdlgui.surfimg. sdl.Surface satisfies draw.Image.

--- sdl/sdl.go
+++ sdl/sdl.go
@@ -255,6 +255,58 @@ func (s *Surface) SetClipRect(r *Rect) {
 	return
 }
 
+func (img *Surface) pixPtr(x, y int) reflect.Value {
+	imglen := int(img.W * img.H)
+	h := reflect.SliceHeader{
+		uintptr(unsafe.Pointer(img.Pixels)),
+		imglen,
+		imglen,
+	}
+
+	switch img.Format.BitsPerPixel {
+	case 8:
+		pix := &(*(*[]uint8)(unsafe.Pointer(&h)))[(y*int(img.W))+x]
+		return reflect.ValueOf(pix).Elem()
+	case 16:
+		pix := &(*(*[]uint16)(unsafe.Pointer(&h)))[(y*int(img.W))+x]
+		return reflect.ValueOf(pix).Elem()
+	case 32:
+		pix := &(*(*[]uint32)(unsafe.Pointer(&h)))[(y*int(img.W))+x]
+		return reflect.ValueOf(pix).Elem()
+	case 64:
+		pix := &(*(*[]uint64)(unsafe.Pointer(&h)))[(y*int(img.W))+x]
+		return reflect.ValueOf(pix).Elem()
+	}
+
+	panic(fmt.Errorf("Image has unexpected BPP: %v", img.Format.BitsPerPixel))
+}
+
+func (img *Surface) ColorModel() image.ColorModel {
+	// TODO: Properly handle various colormodels.
+	return image.RGBAColorModel
+}
+
+func (img *Surface) Bounds() image.Rectangle {
+	return image.Rect(0, 0, int(img.W), int(img.H))
+}
+
+func (img *Surface) At(x, y int) image.Color {
+	var r, g, b, a uint8
+	GetRGBA(uint32(img.pixPtr(x, y).Uint()), img.Format, &r, &g, &b, &a)
+
+	return img.ColorModel().Convert(image.RGBAColor{r, g, b, a})
+}
+
+func (img *Surface) Set(x, y int, c image.Color) {
+	img.Lock()
+	defer img.Unlock()
+
+	r, g, b, a := c.RGBA()
+
+	pix := img.pixPtr(x, y)
+	pix.SetUint(uint64(MapRGBA(img.Format, uint8(r), uint8(g), uint8(b), uint8(a))))
+}
+
 // Map a RGB color value to a pixel format.
 func MapRGB(format *PixelFormat, r, g, b uint8) uint32 {
 	return (uint32)(C.SDL_MapRGB((*C.SDL_PixelFormat)(cast(format)), (C.Uint8)(r), (C.Uint8)(g), (C.Uint8)(b)))
