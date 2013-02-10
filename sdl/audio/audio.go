88d907c020c686c9eb32498b79b1564886c982df
neagix <neagix@gmail.com>
2013-02-10 20:30:59 +0100

    introducing callback-based sound mixing

--- sdl/audio/audio.go
+++ sdl/audio/audio.go
@@ -1,12 +1,16 @@
 /*
+ * Copyright neagix - Feb 2013
  * Copyright: ⚛ <0xe2.0x9a.0x9b@gmail.com> 2010
+ * 
  *
  * The contents of this file can be used freely,
  * except for usages in immoral contexts.
+ * 
  */
 
 /*
-An interface to low-level SDL sound functions.
+An interface to low-level SDL sound functions
+with support for callbacks and rudimentary stream mixing
 */
 package audio
 
@@ -18,7 +22,8 @@ package audio
 // #include "callback.h"
 import "C"
 import "unsafe"
-import "sync"
+import "reflect"
+import "code.google.com/p/rog-go/exp/callback"
 
 // The version of Go-SDL audio bindings.
 // The version descriptor changes into a new unique string
@@ -29,7 +34,18 @@ import "sync"
 //
 // If Go adds some kind of support for package versioning, this function will go away.
 func GoSdlAudioVersion() string {
-	return "⚛SDL audio bindings 1.0"
+	return "Go-SDL audio 1.1"
+}
+
+var userDefinedCallback func(unsafe.Pointer, int)
+
+//export streamCallback
+func streamCallback(arg unsafe.Pointer) {
+	ctx := (*C.context)(arg)
+	
+	// call the actual Go callback defined by user
+	//NOTE: here buffer truncation possible with large NumBytes
+	userDefinedCallback(ctx.Stream, int(ctx.NumBytes))
 }
 
 // Audio format
@@ -57,9 +73,141 @@ type AudioSpec struct {
 	Out_Silence uint8
 	Samples     uint16 // A power of 2, preferrably 2^11 (2048) or more
 	Out_Size    uint32
+	UserDefinedCallback func(unsafe.Pointer, int)
+}
+
+var alreadyOpened	bool
+
+const (
+	// play concatenating samples
+	AE_PLAY_CONCAT = iota
+	//TODO: downmix mode
+	AE_PAUSE
+	AE_UNPAUSE
+)
+
+type AudioEvent struct {
+	Event		int
+	Buffer		[]int16
+}
+
+// Audio status
+const (
+	SDL_AUDIO_STOPPED = C.SDL_AUDIO_STOPPED
+	SDL_AUDIO_PLAYING = C.SDL_AUDIO_PLAYING
+	SDL_AUDIO_PAUSED  = C.SDL_AUDIO_PAUSED
+)
+
+var PlayLoop	chan	AudioEvent
+var PlayQueueSize int
+var TailBuffer []int16
+
+//NOTE: we assume this is not going to be called concurrently :)
+func DownstreamPlayback16(buffer unsafe.Pointer, bufferSize int) {
+
+	// hack to convert C void* pointer to proper Go slice
+	var stream []int16
+	streamLen := bufferSize / 2
+	tailBufferLen := len(TailBuffer)
+	sliceHeader := (*reflect.SliceHeader)((unsafe.Pointer(&stream)))
+	sliceHeader.Cap = streamLen
+	sliceHeader.Len = streamLen
+	sliceHeader.Data = uintptr(buffer)
+
+	// in theory we should overmix samples, but for that a double-buffered approach is best
+	// right now we will just concatenate samples, quirky but serves the purpose of this game
+
+	// first thing, pick any eventual remnants buffer to copy over
+	dstOffset := 0
+	if nil != TailBuffer && tailBufferLen > 0 {
+		for i := range TailBuffer {
+			stream[dstOffset] = TailBuffer[i]
+			dstOffset++
+
+			// we have copied enough
+			if dstOffset == streamLen {
+				newTailBuffer := make([]int16, tailBufferLen-dstOffset)
+				copy(newTailBuffer, TailBuffer[dstOffset:])
+				TailBuffer = newTailBuffer
+				return
+			}
+		}
+
+		// we have consumed the whole TailBuffer, reset it
+		TailBuffer = nil
+	}
+
+	// pick next beep object
+	for {
+		// if there's nothing in queue, just play what we got so far
+		// by default stream is a silence buffer
+		if 0 == PlayQueueSize {
+			return
+		}
+
+		ae := <-PlayLoop
+		
+		switch ae.Event {
+			case AE_UNPAUSE:
+				{
+					PauseAudio(false)
+					continue
+			}
+				case AE_PAUSE:
+				{
+					PauseAudio(true)
+					continue
+				}
+		}
+
+		// prepare eventual tail buffer
+		toBeCopied := streamLen - dstOffset
+		overflowingSamples := len(ae.Buffer) - toBeCopied
+		if overflowingSamples > 0 {
+			TailBuffer = make([]int16, overflowingSamples)
+		}
+
+		copy(stream[dstOffset:], ae.Buffer)
+		if overflowingSamples > 0 {
+			copy(TailBuffer, ae.Buffer[toBeCopied:])
+		}
+	
+				
+		// we have "eaten" a sound object from the queue
+		PlayQueueSize--
+		
+		
+		// this sound object fully satisfied the buffer
+		if overflowingSamples > 0 {
+			return }
+			
+		// in case of perfect boundary match
+		if streamLen == dstOffset + toBeCopied {
+		return }
+			
+		// loop again, pick another sound
+	}
 }
 
 func OpenAudio(desired, obtained_orNil *AudioSpec) int {
+		if alreadyOpened {
+			panic("more than 1 audio stream currently not supported")
+		}
+
+	C.setCallbackFunc(callback.Func)
+	// copy handle to user-defined callback function, if defined
+	// it is perfectly supported to not specify the callback function
+	// in that case you will use default SendAudio semantics
+	// note that if you specify a callback and use SendAudio, a hangup will instead happen
+	// when calling SendAudio	
+	
+	if nil != desired.UserDefinedCallback {
+		userDefinedCallback = desired.UserDefinedCallback
+	} else {
+		userDefinedCallback = DownstreamPlayback16
+		PlayLoop = make(chan AudioEvent)
+	}
+	
 	var C_desired, C_obtained *C.SDL_AudioSpec
 
 	C_desired = new(C.SDL_AudioSpec)
@@ -80,9 +228,7 @@ func OpenAudio(desired, obtained_orNil *AudioSpec) int {
 	status := C.SDL_OpenAudio(C_desired, C_obtained)
 
 	if status == 0 {
-		mutex.Lock()
-		opened++
-		mutex.Unlock()
+		alreadyOpened = true
 	}
 
 	if obtained_orNil != nil {
@@ -100,129 +246,62 @@ func OpenAudio(desired, obtained_orNil *AudioSpec) int {
 }
 
 func CloseAudio() {
-	PauseAudio(true)
-
-	mutex.Lock()
-	{
-		opened--
-		switch {
-		case opened == 0:
-			userPaused = true
-			sdlPaused = true
-		case opened < 0:
+		if !alreadyOpened {
 			panic("SDL audio not opened")
 		}
-	}
-	mutex.Unlock()
 
-	C.callback_unblock()
+	PauseAudio(true)
 
 	C.SDL_CloseAudio()
 }
-
-// Audio status
-const (
-	SDL_AUDIO_STOPPED = C.SDL_AUDIO_STOPPED
-	SDL_AUDIO_PLAYING = C.SDL_AUDIO_PLAYING
-	SDL_AUDIO_PAUSED  = C.SDL_AUDIO_PAUSED
-)
 
 func GetAudioStatus() int {
 	return int(C.SDL_GetAudioStatus())
 }
 
-var opened int = 0
-
-var userPaused bool = true
-var sdlPaused bool = true
-var haveData bool = false
-
-var mutex sync.Mutex
-
 // Pause or unpause the audio.
-// Unpausing is deferred until a SendAudio function receives some samples.
 func PauseAudio(pause_on bool) {
-	mutex.Lock()
-
-	if pause_on != sdlPaused {
 		if pause_on {
-			// Pause SDL audio
-			userPaused = true
-			sdlPaused = true
 			C.SDL_PauseAudio(1)
 		} else {
-			userPaused = false
-			if haveData {
-				// Unpause SDL audio
-				sdlPaused = false
-				C.SDL_PauseAudio(0)
-			} else {
-				// Defer until SendAudio is called
-			}
+			C.SDL_PauseAudio(0)
 		}
+}
+
+// pause or unpause the audio synchronously
+func PauseAudioSync(pause_on bool) {
+	if nil == PlayLoop {
+		panic("Cannot use PauseAudioSync with custom callback")
 	}
-
-	mutex.Unlock()
+		if pause_on {
+			PlayLoop <- AudioEvent{Event:AE_PAUSE}
+		} else {
+			PlayLoop <- AudioEvent{Event:AE_UNPAUSE}
+		}
 }
 
-func LockAudio() {
-	C.SDL_LockAudio()
-}
-
-func UnlockAudio() {
-	C.SDL_UnlockAudio()
-}
 
 // Send samples to the audio device (AUDIO_S16SYS format).
 // This function blocks until all the samples are consumed by the SDL audio thread.
 func SendAudio_int16(data []int16) {
-	if len(data) > 0 {
-		sendAudio((*C.Uint8)(unsafe.Pointer(&data[0])), C.size_t(int(unsafe.Sizeof(data[0]))*len(data)))
-	}
+	PlayQueueSize++
+	PlayLoop <- AudioEvent{Event:AE_PLAY_CONCAT, Buffer: data}
 }
 
 // Send samples to the audio device (AUDIO_U16SYS format).
 // This function blocks until all the samples are consumed by the SDL audio thread.
 func SendAudio_uint16(data []uint16) {
-	if len(data) > 0 {
-		sendAudio((*C.Uint8)(unsafe.Pointer(&data[0])), C.size_t(int(unsafe.Sizeof(data[0]))*len(data)))
-	}
+	panic("Only int16 samples currently supported")
 }
 
 // Send samples to the audio device (AUDIO_S8 format).
 // This function blocks until all the samples are consumed by the SDL audio thread.
 func SendAudio_int8(data []int8) {
-	if len(data) > 0 {
-		sendAudio((*C.Uint8)(unsafe.Pointer(&data[0])), C.size_t(int(unsafe.Sizeof(data[0]))*len(data)))
-	}
+	panic("Only int16 samples currently supported")
 }
 
 // Send samples to the audio device (AUDIO_U8 format).
 // This function blocks until all the samples are consumed by the SDL audio thread.
 func SendAudio_uint8(data []uint8) {
-	if len(data) > 0 {
-		sendAudio((*C.Uint8)(unsafe.Pointer(&data[0])), C.size_t(int(unsafe.Sizeof(data[0]))*len(data)))
-	}
-}
-
-func sendAudio(data *C.Uint8, numBytes C.size_t) {
-	if numBytes > 0 {
-		mutex.Lock()
-		{
-			haveData = true
-
-			if (userPaused == false) && (sdlPaused == true) {
-				// Unpause SDL audio
-				sdlPaused = false
-				C.SDL_PauseAudio(0)
-			}
-		}
-		mutex.Unlock()
-
-		C.callback_fillBuffer(data, numBytes)
-
-		mutex.Lock()
-		haveData = false
-		mutex.Unlock()
-	}
+	panic("Only int16 samples currently supported")
 }
--- sdl/audio/callback.h
+++ sdl/audio/callback.h
@@ -4,12 +4,26 @@
  * The contents of this file can be used freely,
  * except for usages in immoral contexts.
  */
+ 
+#ifndef	__CALLBACK_H
+#define	__CALLBACK_H
 
 #include <SDL_audio.h>
+
+typedef struct context context;
+struct context {
+	void *Stream;
+	int NumBytes;
+};
 
 typedef void (SDLCALL *callback_t)(void *userdata, Uint8 *stream, int len);
 
 extern callback_t callback_getCallback();
-extern void       callback_fillBuffer(Uint8 *data, size_t numBytes);
-extern void       callback_unblock();
+//extern void       callback_fillBuffer(Uint8 *data, size_t numBytes);
+//extern void       callback_unblock();
 
+// from pa.c
+extern void setCallbackFunc(void *cb);
+extern int paStreamCallback(void *outputBuffer, unsigned long bytesCount);
+
+#endif //__CALLBACK_H
